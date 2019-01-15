a4cfbf4527befa4ce4183dabd3ddf590d0c8fadf
khalid01 coor <khalidfawal01@hotmail.com>
2019-01-14 22:46:25 -0500

    Here Is my shit

--- ICS3UCPT/ICS3UCPT/Form1.Designer.cs
+++ ICS3UCPT/ICS3UCPT/Form1.Designer.cs
@@ -100,6 +100,7 @@
             this.button6.TabIndex = 6;
             this.button6.Text = "Khalid";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // checkBox1
             // 
