90b073f84ee246bfda347abb7597b2c1323adb4a
Khaled Alfawal <353042054@educ.dpcdsb.org>
2019-01-14 22:38:33 -0500

    okkminee

--- ICS3UCPT/ICS3UCPT/Form1.Designer.cs
+++ ICS3UCPT/ICS3UCPT/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -90,11 +91,21 @@
             this.label1.TabIndex = 5;
             this.label1.Text = "label1";
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(249, 353);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Zena";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -116,6 +127,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
     }
 }
 
